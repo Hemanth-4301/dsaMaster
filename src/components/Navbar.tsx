88420e4bea7514b73a117234bb06e95d02cf8b8c
Hemanth-4301 <hemanthachar526@gmail.com>
2025-06-03 11:42:06 +0530

    updated data

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,7 +30,6 @@ const Navbar: React.FC<NavbarProps> = ({
 
   const navLinks = [
     { to: "/#hero", label: "Home" },
-    { to: "/cs", label: "CS" },
     { to: "/todo", label: "Todo" },
     { to: "/about", label: "About" },
   ];
